74122a12aedcb4ffb7ecbd934b8357e988b20c9a
jim schotanus <jimschotanus@gmail.com>
2021-04-01 14:12:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let person = {
+  name: 'Jim',
+  age: [33]
+};
+let evaluations = [7, 10, 9];
+
+var favoriteColors = ["red", "blue", "green", 5];
+var total = favoriteColors.push = ["yellow",
+{ greeting: "hi ik ben een object" }];
+
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(evaluations);
+console.log(favoriteColors);
+console.log(favoriteColors[3]);
+console.log(total);
+console.log(favoriteColors.length);
+console.log(favoriteColors.push)
+
+
+
+
+
+
+
+
